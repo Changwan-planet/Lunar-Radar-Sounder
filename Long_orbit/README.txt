83fd99b83f57c3cd1852c6b151e019db8aee8d75
changwan <tjsckddhks@gmail.com>
2021-01-08 15:13:16 +0900

    PLEASE a READEME.txt file

---
+++ Long_orbit/README.txt
@@ -0,0 +1,3 @@
+If you want to print out certain range of the B_SCAN_IMAGE, you can use Long_orbit_LRSOPEN.f90
+You have to  tell your computer where the file ends because your computer is moron.
+With W_Long_orbit_LRSOPEN.f90, you can find where it ends. That is, you can get to know the whoel number of the columns of the B_SCAN_IMAGE. 
---
+++ Long_orbit/W_Long_orbit_LRSOPEN.f90
@@ -0,0 +1,61 @@
+Program Long_orbit_LRSOPEN
+Implicit none
+
+COMPLEX, DIMENSION(1000,1) :: Pixel
+
+REAL :: SC_Lat
+REAL :: SC_Long
+REAL :: SC_Alt
+REAL :: H_offset
+REAL :: H_nadir
+REAL :: Start_range
+REAL, DIMENSION(1000,1) :: Intensity
+
+INTEGER :: TI
+INTEGER :: COL, COL2
+INTEGER :: ALLOCATESTATUS
+INTEGER :: i,j,k
+
+
+OPEN(UNIT=10, FILE="SAR_05km_08831_60600_62399.dat", FORM='UNFORMATTED', STATUS='OLD')
+OPEN(UNIT=20, FILE="W_HEADER_Long_orbit_LRS.txt",STATUS='REPLACE')
+!OPEN(UNIT=21, FILE="INTENSITY_LRS.txt",STATUS='REPLACE')
+!OPEN(UNIT=22, FILE="MAXLOCATION_LRS.txt",STATUS='REPLACE')
+OPEN(UNIT=23, FILE="W_B_SCAN_LRS.txt",STATUS='REPLACE')
+
+!=====INITIALIZATION=======================================
+INTENSITY = 0.0
+j = 0
+k = 0
+!==========================================================
+
+100 Read(10, End=200) TI,SC_Lat,SC_Long,SC_Alt,H_offset,H_nadir,Start_range,Pixel
+  Write(20,*) TI,SC_Lat,SC_Long,SC_Alt,H_offset,H_nadir,Start_range
+      j=j+1
+      Print *, "j=",j
+
+!=====INTENSITY=============================================  
+!  Z=a+ib
+! |Z|=sqrt(a**2+b**2)
+
+    Do i=1,1000
+      Intensity(i,1)=20*log10(ABS(Pixel(i,1))) 
+   End do
+!===========================================================
+
+
+!======Max intensity and location in the intensity array=====
+     
+     !Write(22,*)'Max=',Maxval(Intensity)
+     !Write(22,*)'Maxlocation=',25*Maxloc(Intensity)
+!============================================================ 
+
+    GO TO 100
+200 Close(10)
+    STOP   
+    
+End Program 
+ 
+
+
+
